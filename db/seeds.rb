838adb3d63acb647cbbe2ce3d2b3606e54581afd
Javier Jimenez <javierrcc522@gmail.com>
2017-11-28 12:08:39 -0800

    add home root

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+
+10.times do |index|
+  User.create!(name: Faker::Name.first_name,
+              language: Faker::Hacker.verb,
+              image: Faker::Avatar.image("my-own-slug", "50x50", "jpg"))
+
+end
+p "Created #{User.count} users"
