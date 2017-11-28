ccdb0d084785f4cb01b467906c725ce23be20383
Javier Jimenez <javierrcc522@gmail.com>
2017-11-28 10:40:48 -0800

    add routes to the config folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users do
+    resources :questions
+  end
+
+  resources :questions do
+    resources :answers
+  end
+
 end
